c987f4276ac7d4adb8f64f5c0c2e6b91edc2e591
Ayushi💕 <ayushithummar14@gmail.com>
2026-04-06 15:29:50 +0530

    Added all models - CartItem, Order, OrderItem, Notification

---
+++ Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShelf.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; } = 1;
+
+        // Whose cart
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+
+        // Book in cart (nullable because it could be ebook)
+        public int? BookId { get; set; }
+        public Book? Book { get; set; }
+
+        // Ebook in cart (nullable because it could be book)
+        public int? EbookId { get; set; }
+        public Ebook? Ebook { get; set; }
+    }
+}
---
+++ Models/Notification.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShelf.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        // "order", "offer", "system"
+        public string Type { get; set; } = "system";
+
+        // e.g. "2 mins ago", "Just now", "Yesterday"
+        public string TimeAgo { get; set; } = "Just now";
+
+        // true = unread, false = read
+        public bool IsRead { get; set; } = false;
+
+        // false = not dismissed, true = dismissed
+        public bool IsDismissed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Which user gets this notification
+        // null = shown to ALL users (admin broadcast)
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+    }
+}
